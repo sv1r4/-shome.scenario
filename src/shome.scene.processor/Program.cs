07eee3869bd5f4c0ac0ef44f19b5222fc82d4da3
Yury Svirko <sv1r4.sd@gmail.com>
2018-12-02 16:21:31 +0300

    ci stack variable

--- src/shome.scene.processor/Program.cs
+++ src/shome.scene.processor/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using shome.scene.provider.contract;
 
 namespace shome.scene.processor
 {
@@ -6,7 +10,21 @@ namespace shome.scene.processor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ss = new ServiceCollection();
+            ss.AddTransient<ISceneProvider, TestSceneProvider>();
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            var config = builder.Build();
+            Console.WriteLine("Scene Processor");
+
         }
+    }
+
+    public class TestSceneProvider : ISceneProvider
+    {
+
     }
 }
