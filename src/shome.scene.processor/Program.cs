58d07ea8796e76dfedc0c567d0efab23ee54f8b8
Yury Svirko <sv1r4.sd@gmail.com>
2018-12-05 00:25:08 +0300

    poc mqttnet lib

--- src/shome.scene.processor/Program.cs
+++ src/shome.scene.processor/Program.cs
@@ -1,30 +1,52 @@
 ﻿using System;
 using System.IO;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Extensions.ManagedClient;
 using shome.scene.mqtt.config;
-using shome.scene.mqtt.extensions;
 using shome.scene.provider.contract;
 using shome.scene.provider.yml;
 using shome.scene.provider.yml.config;
-using uPLibrary.Networking.M2Mqtt;
 
 namespace shome.scene.processor
 {
-    class Program
+    public class Program
     {
         private static ILogger _logger;
 
-        static void Main()
+        public static void Main()
         {
-            MainAsync().GetAwaiter().GetResult();
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, args) =>
+            {
+                Console.WriteLine("App graceful stop");
+                try
+                {
+                    Stop().GetAwaiter().GetResult();
+                }
+                finally
+                {
+                    cts.Cancel();
+                }
+            };
+            Console.WriteLine("App start");
+
+            Start().Wait(cts.Token);
         }
 
-        static async Task MainAsync()
+        private static Task Stop()
+        {
+            return Task.CompletedTask;
+        }
+
+        private static async Task Start()
         {
             try
             {
@@ -41,7 +63,6 @@ namespace shome.scene.processor
                     logging.AddConfiguration(config.GetSection("Logging"));
                     logging.AddConsole();
                 });
-                services.AddMqttClient(config);
                 services.AddSingleton<ISceneProvider, YamlSceneProvider>();
                 var fp = new PhysicalFileProvider(cfg.DirectoryAbsolute);
                 services.AddSingleton<IFileProvider>(fp);
@@ -49,14 +70,7 @@ namespace shome.scene.processor
                 var sp = services.BuildServiceProvider();
                 _logger = sp.GetService<ILogger<Program>>();
 
-                var mqtt = sp.GetRequiredService<MqttClient>();
-                var mqttConfig = sp.GetRequiredService<IOptions<MqttConfig>>().Value;
-
-                mqtt.Connect($"scene.processor-{Guid.NewGuid()}", mqttConfig.User, mqttConfig.Password);
-
-                
-
-                _logger.LogInformation($"MqtConnected {mqtt.IsConnected}");
+                await InitMqtt(config);
 
                 _logger.LogInformation("Scene Processor");
                 await InfinitMonitorChanges(fp, "*.yaml");
@@ -65,6 +79,48 @@ namespace shome.scene.processor
             {
                 Console.WriteLine(ex);
             }
+        }
+
+        private static async Task InitMqtt(IConfigurationRoot config)
+        {
+            var cfg = config.GetSection(nameof(MqttConfig)).Get<MqttConfig>();
+            var optionBuilder = new MqttClientOptionsBuilder()
+                .WithClientId($"shome.scene.processor-{Guid.NewGuid()}")
+                .WithTcpServer(cfg.Host, cfg.Port);
+
+            if (!string.IsNullOrWhiteSpace(cfg.User))
+            {
+                optionBuilder = optionBuilder.WithCredentials(cfg.User, cfg.Password);
+            }
+
+            var options = new ManagedMqttClientOptionsBuilder()
+                .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+                .WithClientOptions(optionBuilder.Build())
+                .Build();
+
+            var mqttClient = new MqttFactory().CreateManagedMqttClient();
+            await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("#").WithAtLeastOnceQoS().Build());
+            mqttClient.Connected += (sender, args) =>
+            {
+                _logger.LogDebug("mqtt connected"); 
+            };
+
+            mqttClient.ConnectingFailed += (sender, args) =>
+            {
+                _logger.LogError($"mqtt connection fail. Error {args.Exception}");
+            };
+            mqttClient.SynchronizingSubscriptionsFailed += (sender, args) =>
+            {
+                _logger.LogError($"mqtt sync subscriptions fail. Error {args.Exception}");
+            };
+
+            mqttClient.ApplicationMessageReceived += (sender, args) =>
+            {
+                _logger.LogDebug($"message received.\n\ttopic='{args.ApplicationMessage.Topic}'\n\tmessage='{Encoding.UTF8.GetString(args.ApplicationMessage.Payload)}'");
+            };
+
+            await mqttClient.StartAsync(options);
+            //await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("my/topic2").Build());
         }
 
         private static async Task InfinitMonitorChanges(IFileProvider fp, string path)
